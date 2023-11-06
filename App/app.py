8f3f16183d7c08d03a7242a2784c460c68337e06
GhOsTxx001 <tyegabe001@gmail.com>
2023-11-06 13:32:20 +0300

    inventory fucntions, food distro, weekly schedule

--- App/app.py
+++ App/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
+from food_distribution import get_food_data, calculate_distribution, send_notifications
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Welcome to the Food Security App'
-#AUTHENTICATION AND WHAT NOT
-#signing up
+
+# AUTHENTICATION AND WHAT NOT
+# signing up
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -33,6 +37,27 @@ def signin():
             return jsonify({'message': 'Invalid username or password'}), 401
     else:
         return jsonify({'message': 'Username and password are required'}), 400
+
+# FOOD DISTRIBUTION
+@app.route('/distribute_food', methods=['POST'])
+def distribute_food():
+    data = request.get_json()
+    location = data.get('location')
+    food_data = get_food_data(location)
+    distribution = calculate_distribution(food_data)
+    send_notifications(distribution)
+    return jsonify({'message': 'Food distribution successful'}), 200
+
+    #scheduling weekly
+    scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=distribute_food,
+    trigger=IntervalTrigger(weeks=1),
+    id='distribute_food_job',
+    name='Distribute food every week',
+    replace_existing=True)
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     app.run(debug=True)
